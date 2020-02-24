587fcefbfe0d25ef57386b4a252306f7f3db7f20
Tanner Leach <tanner.leach90@gmail.com>
2020-02-23 19:39:02 -0700

    latest commit with comments and readme

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # weatherapi
 
-ran into some trouble last night. will complete tomorrow or whenever I can get to it
+Completed
 
 https://tannerleach90.github.io/weatherapi/
+
+After typing out necessary HTML for basic formatting, used AJAX calls using API keys to find necessary data for the user depending on US City input into field. When data comes back, decided to build the data and the html and appending into the container HTML. Used a forEach loops in the second half of script to build an array of 5 weather data objects to create 5-day future forecast. 
+
+For the search history, ul was made so that each time a new city was input, we create a new li with the data and city name and append to the said ul.
--- script.js
+++ script.js
@@ -1,27 +1,28 @@
-
+//Makes sure code runs when DOM is loaded
 $(function () {
     var $citySearch = $('#city-search');
     var $cityHistory = $('#city-history');
-
+//when form submits, grab city from input value and call city function with value
     $("#weather-form").submit(function (event) {
         event.preventDefault();
         var city = $citySearch.val();
 
         callByCity(city);
     });
-
+//API endpoint called with API key (so we can access API) with imperial query param conversion added to URL
     function callByCity(city) {
         var corsProxy = "https://cors-anywhere.herokuapp.com/";
         var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q="        
-        $.get(corsProxy + apiUrl + city + "&appid=054de0bb639f081ad717b08a3b441921", function (data) {        
+        $.get(corsProxy + apiUrl + city + "&appid=054de0bb639f081ad717b08a3b441921&units=imperial", function (data) {        
             $(".main-city-data").html(
                 "<h2>" + data.name + " " + "(" + moment().format("MMM Do YY") + ")" + "<i class='fas fa-cloud'></i></h2>" +
-                "<p>Temperature: " + data.main.temp + "<p>" +
+                "<p>Temperature: " + data.main.temp + "F<p>" +
                 "<p>Humidity: " + data.main.humidity + "%<p>" + 
                 "<p>Wind Speed: " + data.wind.speed + "MPH<p>" + 
                 "<p><span class='cloud-cover'>Cloud Cover:</span>"  + " " + data.clouds.all + "<p>"                 
             )
-
+//made empty ul in the html. after data comes back and we create above html, we take name of city from the data and create an li 
+//with city name and append to ul
             $cityHistory.append(
                 '<li>' + data.name + '</li>'
             )        
@@ -29,20 +30,20 @@ $(function () {
         futureForecast(city);
         });
     }
-
+//similar AJAX call except put into forEACH loop to get extended forecast data
     function futureForecast(city) {
         var corsProxy = "https://cors-anywhere.herokuapp.com/";
         var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q="
         var string = "";
-        $.get(corsProxy + apiUrl + city + "&appid=054de0bb639f081ad717b08a3b441921&cnt=5", function (data) {
-            
+        $.get(corsProxy + apiUrl + city + "&appid=054de0bb639f081ad717b08a3b441921&cnt=5&units=imperial", function (data) {
+            //when data received, build out HTML with data and append to container to display said data
             data.list.forEach(function (forecast) {
                 console.log(forecast)
                 string += "<div class='forecast-city'>" + city +
                     "<h3>" + moment().format("MMM Do YY") + "</h3>" +
                     "<i class='fas fa-sun'></i>" +
-                    "<p>" + forecast.main.temp + "</p>" +
-                    "<p>" + forecast.main.humidity + "</p>" + "</div>"                                    
+                    "<p>" + forecast.main.temp + "F</p>" +
+                    "<p>" + forecast.main.humidity + "%</p>" + "</div>"                                    
             })
 
             $('#forecast-city-container').html(string);
